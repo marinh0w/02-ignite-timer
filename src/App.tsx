1599174dd795db9559c415cbfca104e7dbe07d61
marinh0w <gabrielgambardella14@gmail.com>
2023-02-07 19:53:13 -0300

    Cap 2 concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-import { useState } from "react";
+import { useState } from 'react'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
-    </>
-  );
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
