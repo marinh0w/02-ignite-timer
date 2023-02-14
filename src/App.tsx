9ba867adf286470e8c3b6eb6845ff7f995d0566e
marinh0w <gabrielgambardella14@gmail.com>
2023-02-14 18:33:45 -0300

    cap 2 aula 1 routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-
+import { BrowserRouter } from 'react-router-dom'
 import { useState } from 'react'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+      <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
